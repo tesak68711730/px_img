2af19fad6715957b9ad47f710e4ea7628165580f
justin_st1 <justin_st1@mail.ru>
2022-05-30 12:09:22 +0300

    update to last version

--- src/main/java/com/example/imagetest/ImageTestApplication.java
+++ src/main/java/com/example/imagetest/ImageTestApplication.java
@@ -5,128 +5,112 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
-import java.awt.image.PixelGrabber;
-import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 @SpringBootApplication
 public class ImageTestApplication {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 //        SpringApplication.run(ImageTestApplication.class, args);
 
-        FileWriter writer = new FileWriter("src/main/resources/pixel_values.txt");
-        //Reading the image
-        File file= new File("C:\\Users\\TT\\Desktop\\71645695_p0_master1200.jpg");
-        BufferedImage img = ImageIO.read(file);
+        File file = new File("C:\\Users\\TT\\Desktop\\71645695_p0_master1200.jpg");
+        List<Color> colorList = parseImg(file);
 
+        BufferedImage img;
+        try {
+            img = ImageIO.read(file);
+            generateImg(img, colorList);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static List<Color> parseImg (File file) {
+        FileWriter writer;
         List<Color> colorList = new ArrayList<>();
 
-        for (int y = 0; y < img.getHeight(); y++) {
-            for (int x = 0; x < img.getWidth(); x++) {
-                //Retrieving contents of a pixel
-                int pixel = img.getRGB(x,y);
-                //Creating a Color object from pixel value
-                Color color = new Color(pixel, true);
-                colorList.add(color);
-                //Retrieving the R G B values
-                int red = color.getRed();
-                int green = color.getGreen();
-                int blue = color.getBlue();
-                writer.append(red+":");
-                writer.append(green+":");
-                writer.append(blue+" ");
-                writer.flush();
+        try {
+            writer = new FileWriter("src/main/resources/pixel_values.txt");
+            BufferedImage img = ImageIO.read(file);
+
+            for (int y = 0; y < img.getHeight(); y++) {
+                for (int x = 0; x < img.getWidth(); x++) {
+                    //Retrieving contents of a pixel
+                    int pixel = img.getRGB(x, y);
+                    //Creating a Color object from pixel value
+                    Color color = new Color(pixel, true);
+                    colorList.add(color);
+                    //Retrieving the R G B values
+                    int red = color.getRed();
+                    int green = color.getGreen();
+                    int blue = color.getBlue();
+                    writer.append(String.valueOf(red)).append(":");
+                    writer.append(String.valueOf(green)).append(":");
+                    writer.append(String.valueOf(blue)).append(" ");
+                    writer.flush();
+                }
+                writer.append("\n");
             }
-            writer.append("\n");
+            writer.close();
+            System.out.println("RGB values at each pixel are stored in the specified file");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        writer.close();
-        System.out.println("RGB values at each pixel are stored in the specified file");
+        return colorList;
+    }
 
-
-        /*int width, height;
-        File file= new File("C:\\Users\\TT\\Desktop\\71645695_p0_master1200.jpg");
-        BufferedImage source = ImageIO.read(file);
-        width = source.getWidth();
-        height = source.getHeight();
-        getImageFromArray(convertToPixels(source), width, height);*/
-
-
+    public static void generateImg(BufferedImage img, List<Color> colorList) {
         int width = img.getWidth();
         int height = img.getHeight();
-//create buffered image object img
-        BufferedImage imgOut = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-//file object
-        File f = null;
 
-        for(int y = 0; y < height; y++){
-            List<Color> subList = colorList.subList(y * width, y * width + width);
-            for(int x = 0; x < subList.size(); x++){
-                int a = subList.get(x).getAlpha(); //alpha
-                int r = subList.get(x).getRed(); //red
-                int g = subList.get(x).getGreen(); //green
-                int b = subList.get(x).getBlue(); //blue
-                int p = (a<<24) | (r<<16) | (g<<8) | b;
-                img.setRGB(x, y, p);
+        BufferedImage imgOut = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        File f;
+
+        if (colorList.size() != 0) {
+            // get pixel on format (219:4:35)
+            for (int y = 0; y < height; y++) {
+                List<Color> subList = colorList.subList(y * width, y * width + width);
+                for (int x = 0; x < subList.size(); x++) {
+                    int a, r, g, b, p;
+                    if (y == x) {
+                        a = 0;
+                        r = 0;
+                        g = 0;
+                        b = 0;
+                    } else {
+                        a = subList.get(x).getAlpha(); //alpha
+                        r = subList.get(x).getRed(); //red
+                        g = subList.get(x).getGreen(); //green
+                        b = subList.get(x).getBlue(); //blue
+                    }
+                    p = (a << 24) | (r << 16) | (g << 8) | b;
+                    imgOut.setRGB(x, y, p);
+                }
+            }
+        } else {
+            // random generation
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    int a = (int) (Math.random() * 256); //alpha
+                    int r = (int) (Math.random() * 256); //red
+                    int g = (int) (Math.random() * 256); //green
+                    int b = (int) (Math.random() * 256); //blue
+                    int p = (a << 24) | (r << 16) | (g << 8) | b;
+                    imgOut.setRGB(x, y, p);
+                }
             }
         }
 
-        try{
+        try {
             f = new File("D:\\Output.jpg");
             ImageIO.write(imgOut, "png", f);
-        }catch(IOException e){
-            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
-
-    }
-
-    public static int[] convertToPixels(Image img) {
-        int width = img.getWidth(null);
-
-        int height = img.getHeight(null);
-        int[] pixel = new int[width * height];
-
-        PixelGrabber pg = new PixelGrabber(img, 0, 0, width, height, pixel, 0, width);
-        try {
-            pg.grabPixels();
-        } catch (InterruptedException e) {
-            throw new IllegalStateException("Error: Interrupted Waiting for Pixels");
-        }
-        if ((pg.getStatus() & ImageObserver.ABORT) != 0) {
-            throw new IllegalStateException("Error: Image Fetch Aborted");
-        }
-        return pixel;
-    }
-
-    public static Image getImageFromArray(int[] pixels, int width, int height) throws IOException {
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        WritableRaster raster = (WritableRaster) image.getData();
-        raster.setPixels(0, 0, width, height, pixels);
-        File output = new File("C:\\out.png");
-        ImageIO.write(image, "png", output);
-        System.out.print("written");
-        return image;
-    }
-
-    public static int generateIntNumber(int max) {
-        return new Random().nextInt(max);
-    }
-
-    public static String getSubString(int start, int end, String str) {
-        if (!str.isEmpty()) {
-            return str.substring(start, end);
-        } else {
-            return "";
-        }
-    }
-
-    public static String getSomething() {
-        return "Hello world!";
     }
 }
